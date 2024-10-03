e53a7ffa3f81e5fabb23f4707c7ea6d42a80c1a6
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-10-03 11:24:19 +0100

    created a password-strength-checker component

--- src/components/PasswordStrengthChecker.js
+++ src/components/PasswordStrengthChecker.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const PasswordStrengthChecker = () => {
   const [password, setPassword] = useState("");
@@ -8,25 +7,32 @@ const PasswordStrengthChecker = () => {
   const handleInputChange = (event) => {
     event.preventDefault();
     setPassword(event.target.value);
-    handleStrengthChange(event.target.value)
+    handleStrengthChange(event.target.value);
   };
+
   const handleStrengthChange = (password) => {
     if (password.length <= 3) {
-      setStrength("Weak");
+      setStrength("weak");
     } else if (password.length >= 4 && password.length <= 7) {
-      setStrength("Medium");
+      setStrength("medium");
     } else {
-      setStrength("Strong");
+      setStrength("strong");
     }
   };
 
   return (
     <section>
-      <h1>Password-Strength Checker</h1>
-      <input type="password" placeholder="Enter password" value={password} onChange={handleInputChange} />
-      <p>{strength}</p>
+      <h1>Password Strength Checker</h1>
+      <input
+        type="password"
+        placeholder="Enter password"
+        value={password}
+        onChange={handleInputChange}
+      />
+      <p className={strength}>{strength.charAt(0).toUpperCase() + strength.slice(1)}</p>
     </section>
   );
 };
 
 export default PasswordStrengthChecker;
+
